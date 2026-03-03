e8117121da93b381551e860d48a79137ac3c0618
damasceno1997 <danilo.damaceno@ba.estudante.senai.br>
2026-03-03 16:55:57 -0300

    Add files via upload

--- atividade2.py
+++ atividade2.py
@@ -1,20 +1,3 @@
 import os
 
-#limpa o terminal
-
-os.system("cls || clear")
-
-
-numero = int(input("digite um numero:10"))
-
-if numero <10:
-    print ("\no numero é menor que 10.")
-
-if numero > 10:
-    print ("\no numero é maior que 10.")
-
-if numero  == 10: 
-    print ("\no numero é igual a 10.")
-
-print ("\nfim do programa")
-
+os.system
